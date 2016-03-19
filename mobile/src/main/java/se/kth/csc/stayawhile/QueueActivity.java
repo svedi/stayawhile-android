cd922ce406b8a3e7d7821303b69ae7756f623734
Johan Sannemo <johan.sannemo@gmail.com>
2016-03-19 13:35:57 +0100

    Add logout menu

--- mobile/src/main/java/se/kth/csc/stayawhile/QueueActivity.java
+++ mobile/src/main/java/se/kth/csc/stayawhile/QueueActivity.java
@@ -1,16 +1,12 @@
 package se.kth.csc.stayawhile;
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.ContextMenu;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -264,30 +260,8 @@ public class QueueActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- mobile/src/main/java/se/kth/csc/stayawhile/QueueListActivity.java
+++ mobile/src/main/java/se/kth/csc/stayawhile/QueueListActivity.java
@@ -1,5 +1,6 @@
 package se.kth.csc.stayawhile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,6 +11,8 @@ import android.view.MenuItem;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+
+import se.kth.csc.stayawhile.cookies.PersistentCookieStore;
 
 public class QueueListActivity extends AppCompatActivity {
 
@@ -51,23 +54,21 @@ public class QueueListActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_logout) {
+            new PersistentCookieStore(getApplicationContext()).removeAll();
+            Intent queueList = new Intent(this, MainActivity.class);
+            queueList.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(queueList);
+            finish();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
